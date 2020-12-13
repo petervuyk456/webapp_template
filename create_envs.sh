e5f38aeda45760d1be460ffd042b74a1601253c1
Peter Vuyk <peter.vuyk456@gmail.com>
2020-12-13 18:20:58 -0500

    Fixed create_env.sh

--- create_envs.sh
+++ create_envs.sh
@@ -2,7 +2,7 @@
 cd environments
 
 [ ! -f ".env.mongo" ] && touch .env.mongo
-[ ! -f ".env.node" ] && touch .env.api
+[ ! -f ".env.node" ] && touch .env.node
 [ ! -f ".env.api" ] && touch .env.api
 
 cd ..
