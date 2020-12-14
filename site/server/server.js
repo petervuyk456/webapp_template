2dae8afba9563bd9169a711402775d743eeebfc8
Peter Vuyk <peter.vuyk456@gmail.com>
2020-12-14 11:44:56 -0500

    Split node container into node & react separate containers. Fixed Dockerfiles and docker-compose for api container and node/react containers.

---
+++ site/server/server.js
@@ -0,0 +1,23 @@
+import path from 'path';
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/api/test', (req, res) => {
+    res.json([
+        { name: 'test1', how: "just like this"}
+    ]);
+  });
+
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+        
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
+  
+app.listen(port, () => console.log(`Listening on port ${port}`));
