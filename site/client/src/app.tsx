4308db5b9cdeec97cd8b26fee7df9c9399a1aa49
craitano <craitano>
2020-12-20 18:50:39 -0600

    Add typescript support

--- site/client/src/app.tsx
+++ site/client/src/app.tsx
@@ -3,4 +3,13 @@ import { render } from 'react-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
+function test() {
+    let a =""
+    return a
+}
+
+function test2() {
+    const x = test()
+}
+
 render(<h1>Hello World!</h1>, document.getElementById('root'));
--- site/client/webpack.config.js
+++ site/client/webpack.config.js
@@ -2,7 +2,9 @@ const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src', 'app.js'),
+  entry: path.resolve(__dirname, 'src', 'app.tsx'),
+  // Generate source-maps for debugging
+  devtool: 'inline-source-map',
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
@@ -10,27 +12,28 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.[t|j]sx?$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
+        use: ['babel-loader', 'ts-loader'],
       },
       {
         test: /\.(s?)css$/,
-        use: [
-          {
-            loader: 'style-loader',
-          },
-          {
-            loader: 'css-loader',
-          },
-          {
-            loader: 'sass-loader',
-          },
-        ],
+        use: [ 'style-loader', 'css-loader', 'sass-loader'],
       },
+      // Add support for non-typed js modules
+      {
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: "javascript/auto"
+      }
     ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.mjs', '.js', '.jsx' ],
+    // Setup absolute paths to common folders
+    alias: {
+      components: path.resolve(__dirname, "src/components/")
+    }
   },
   plugins: [
     new HtmlWebPackPlugin({
